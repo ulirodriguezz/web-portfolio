0ffe3996e997ac5f18280de92e5286d8d5abf974
ulises <ulirodrigueze@gmail.com>
2025-05-05 22:45:30 -0300

    Updating Frameworks section

--- src/components/Home/Profile.jsx
+++ src/components/Home/Profile.jsx
@@ -20,10 +20,39 @@ function Profile(){
                     <div className={styles.viteReactDiv}>
                         <NamedImage imgSource='../../../src/assets/react.png' alt='React logo' name='React'/>
                         <span>+</span>
-                        <NamedImage imgSource='../../../src/assets/vite.png' alt='React logo' name='Vite'/>   
+                        <NamedImage imgSource='../../../src/assets/vite.png' alt='Vite logo' name='Vite'/>   
                     </div>
                     <p className='my-p'>
-                        When developing Frontend applications, i use React as a Framework with Vite as a package manager.
+                        When developing web applications, I use React as a Framework with Vite as a bundler. For example, i made this site you are currently on with React + Vite
+                    </p>
+                    <div className={styles.viteReactDiv}>
+                        <NamedImage imgSource='../../../src/assets/js.png' alt='JS logo' name='JavaScript'/>
+                        <span>+</span>
+                        <NamedImage imgSource='../../../src/assets/html.png' alt='HTML logo' name='HTML'/>
+                        <span>+</span>
+                        <NamedImage imgSource='../../../src/assets/css.png' alt='CSS logo' name='CSS'/>
+                        <span>+</span>
+                        <NamedImage imgSource='../../../src/assets/sass.png' alt='SASS logo' name='SASS'/>
+                    </div>
+                    <p className='my-p'>
+                        When it comes to programming lenguages, i use the ones shown above. <br />
+                        I use JavaScript to to make sites dynamic with events and functions as well as
+                        programming the logic for the application. <br />
+                        For the structure of the site/webapp I use HTML, paying close attention to SEO best practices. <br />
+                        For styling, i use CSS and SASS on top of some libraries like Bootstrap or other component libraries.
+                    </p>
+                    <div className={styles.viteReactDiv}>
+                        <NamedImage imgSource='../../../src/assets/bootstrap.png' alt='Bootstrap logo' name='Bootstrap'/>
+                        <span>+</span>
+                        <NamedImage imgSource='../../../src/assets/swal.png' alt='Sweet Alert logo' name='Sweet Alert'/>
+                        <span>+</span>
+                        <NamedImage imgSource='../../../src/assets/react-router.svg' alt='React Router logo' name='React Router'/>
+                    </div>
+                    <p className='my-p'>
+                        Finally, these are some of the libraries i often use in my projects. <br />
+                        I use React Router to program the routing and navigation of my React applications. <br />
+                        I use Sweet Alert and Tostify for alerts and notifications. <br />
+                        I use Bootstrap for some of its pre-made components and styles.
                     </p>
                 </CustomTabItem>
                 <CustomTabItem name="Backend">
