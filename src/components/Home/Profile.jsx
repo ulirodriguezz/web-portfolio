5a45eeb367a048338013858bd10e42a5f2cdd068
ulises <ulirodrigueze@gmail.com>
2025-05-05 23:23:25 -0300

    React icons and improveing Framework section

--- src/components/Home/Profile.jsx
+++ src/components/Home/Profile.jsx
@@ -3,12 +3,15 @@ import CustomTabs from '../Common/CustomTabs'
 import CustomTabItem from '../Common/CutomTabItem'
 import NamedImage from '../Common/NamedImage/NamedImage'
 import styles from './Home.module.scss'
+import { SiFramework } from "react-icons/si";
+import { MdOutlineWeb } from "react-icons/md";
+import { IoLibrary } from "react-icons/io5";
 function Profile(){
     return(
         <section className={styles.myProfile}>
             <h2 className='my-h2'>My Profile</h2>
             <p className='my-p'>
-                As a student of software engenieering, i have leart to develop scalable solutions for both backend and frontend.
+                As a student of software engenieering, I have learnt to develop scalable solutions for both backend and frontend.
                 I have developed several Rest APIs with Spring boot as a framework and Web Apps and SPAs with React.
                 <br />
                 I am capable of developing web applications from end to end including APIs and Databases (SQL and noSQL).
@@ -17,14 +20,17 @@ function Profile(){
             <CustomTabs defaultTabName = "Frontend">
                 <CustomTabItem name="Frontend">
                     <h4 className='my-h4'>Frontend Stack</h4>
+                    <p>These are the tools and technologies I use when building modern web applications.</p>
+                    <h5 className='my-h5'><SiFramework className={styles.reactIcons}/> Frameworks and Bundlers</h5>
                     <div className={styles.viteReactDiv}>
                         <NamedImage imgSource='../../../src/assets/react.png' alt='React logo' name='React'/>
                         <span>+</span>
                         <NamedImage imgSource='../../../src/assets/vite.png' alt='Vite logo' name='Vite'/>   
                     </div>
                     <p className='my-p'>
-                        When developing web applications, I use React as a Framework with Vite as a bundler. For example, i made this site you are currently on with React + Vite
+                        When developing web applications, I use React as a Framework with Vite as a bundler. For example, I made this site you're browsing now with React + Vite
                     </p>
+                    <h5 className='my-h5'><MdOutlineWeb className={styles.reactIcons}/> Core Web Technologies</h5>
                     <div className={styles.viteReactDiv}>
                         <NamedImage imgSource='../../../src/assets/js.png' alt='JS logo' name='JavaScript'/>
                         <span>+</span>
@@ -35,12 +41,13 @@ function Profile(){
                         <NamedImage imgSource='../../../src/assets/sass.png' alt='SASS logo' name='SASS'/>
                     </div>
                     <p className='my-p'>
-                        When it comes to programming lenguages, i use the ones shown above. <br />
-                        I use JavaScript to to make sites dynamic with events and functions as well as
+                        When it comes to programming languages, I use the technologies shown above. <br />
+                        JavaScript is my prefered language to make sites dynamic with events and functions as well as
                         programming the logic for the application. <br />
-                        For the structure of the site/webapp I use HTML, paying close attention to SEO best practices. <br />
-                        For styling, i use CSS and SASS on top of some libraries like Bootstrap or other component libraries.
+                        For the structure of the site/webapp, I use HTML, paying close attention to SEO best practices. <br />
+                        For styling, I use CSS and SASS, often combined with Bootstrap or other component libraries.
                     </p>
+                    <h5 className='my-h5'><IoLibrary className={styles.reactIcons}/> UI & Utility Libraries</h5>
                     <div className={styles.viteReactDiv}>
                         <NamedImage imgSource='../../../src/assets/bootstrap.png' alt='Bootstrap logo' name='Bootstrap'/>
                         <span>+</span>
