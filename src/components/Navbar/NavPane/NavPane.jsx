c29c2fa94595ff74ec23b96bcb8e32a3606ceace
ulises <ulirodrigueze@gmail.com>
2025-05-10 00:42:36 -0300

    Set up routing with react router in main.jsx and NavPane.jsx

--- src/components/Navbar/NavPane/NavPane.jsx
+++ src/components/Navbar/NavPane/NavPane.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './NavPane.module.scss';
 
 function NavPane() {
@@ -6,10 +7,10 @@ function NavPane() {
       <h1 className={`'my-h1' ${styles.navTitle}`}> Portfolio</h1>
       <nav className={styles.navPane} aria-label="Main navigation">
         <ul className={styles.navUl}>
-          <li className={styles.navLi}><a href="#perfil" className={styles.link}>Perfil</a></li>
-          <li className={styles.navLi}><a href="#contacto" className={styles.link}>Contacto</a></li>
-          <li className={styles.navLi}><a href="#home" className={styles.link}>Home</a></li>
-          <li className={styles.navLi}><a href="#portfolio" className={styles.link}>Portfolio</a></li>
+          <li className={styles.navLi}><NavLink to="/" className={styles.link}>Home</NavLink></li>
+          <li className={styles.navLi}><NavLink to="/profile" className={styles.link}>Profile</NavLink></li>
+          <li className={styles.navLi}><NavLink to="/contact" className={styles.link}>Contact</NavLink></li>
+          <li className={styles.navLi}><NavLink to="/projects" className={styles.link}>Projects</NavLink></li>
         </ul>
       </nav>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,19 @@ import Home from './components/Home/Home.jsx'
 import './index.scss'
 import './styles/bootstrap-custom.scss'
 import Navbar from './components/Navbar/Navbar.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import BackendStack from './components/Home/Stacks/BackendStack.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
-    <Home/>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route  path='/contact' element={<></>}/>
+        <Route  path='/profile' element={<></>}/>
+        <Route  path='/projects' element={<></>}/>
+        
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
