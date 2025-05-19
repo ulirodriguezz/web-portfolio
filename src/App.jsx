5c2b271d832a1449c7e04430335cc62b802f4e34
ulirodriguezz <ulirodrigueze@gmail.com>
2025-05-19 20:40:10 -0300

    Added toastify. Toast alert for missing fields in contact form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/Home/Home';
 import ContactContainer from './components/Contact/ContactContainer';
 import Navbar from './components/Navbar/Navbar';
 import emailjs from '@emailjs/browser';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   const [theme, setTheme] = useState('dark');
@@ -11,12 +13,12 @@ function App() {
 
   useEffect(() => {
     emailjs.init({
-  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-  limitRate: {
-    id: 'app',
-    throttle: 10000,
-  },
-});
+      publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+      limitRate: {
+        id: 'app',
+        throttle: 10000,
+      },
+    });
   }, []);
   useEffect(() => {
     document.body.classList.remove('theme-dark', 'theme-light');
@@ -25,6 +27,16 @@ function App() {
 
   return (
     <>
+    <ToastContainer 
+        position="bottom-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick={false}
+        pauseOnHover={true}
+        draggable={true}
+        progress={undefined}
+        theme="dark"
+      />
       <Navbar />
       <Routes location={location}>
         <Route path='/' element={<Home setTheme={setTheme} />} />
--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './Contact.module.scss'
 import LabeledTextInput from './LabeledTextInput/LabeledTextInput';
+import { toast } from 'react-toastify';
 function ContacForm({ onSubmit }) {
 
     const [formErrors, setFormErrors] = useState({})
@@ -18,7 +19,9 @@ function ContacForm({ onSubmit }) {
         setFormErrors(newFormErrors)
 
         if (Object.keys(newFormErrors).length > 0) {
-            alert('Missing Fields')
+            toast.error('Missing fields',{
+                className:styles.toastAlert
+            });
             return
         }
         const msg = {
@@ -35,11 +38,11 @@ function ContacForm({ onSubmit }) {
     }
     return (
         <form action="submit" className={styles.contactForm} onSubmit={handleSubmit}>
-            <LabeledTextInput name='name' id='name-input' label='Name' placeholder='Enter your name' error={formErrors.name ? true : false} onChange={()=>clearError('name')} />
-            <LabeledTextInput name='company' id='company-input' label='Company (Optional)' placeholder='Enter your comapny name' error={formErrors.company ? true : false} onChange={()=>clearError('company')} />
-            <LabeledTextInput name='email' id='email-input' label='Email Adress' placeholder='Enter your email' type='email' error={formErrors.email ? true : false} onChange={()=>clearError('email')} />
+            <LabeledTextInput name='name' id='name-input' label='Name' placeholder='Enter your name' error={formErrors.name ? true : false} onChange={() => clearError('name')} />
+            <LabeledTextInput name='company' id='company-input' label='Company (Optional)' placeholder='Enter your comapny name' error={formErrors.company ? true : false} onChange={() => clearError('company')} />
+            <LabeledTextInput name='email' id='email-input' label='Email Adress' placeholder='Enter your email' type='email' error={formErrors.email ? true : false} onChange={() => clearError('email')} />
             <LabeledTextInput name='message' id='message-input' label='Message'
-                placeholder="Write your message here" type='email' size='big' error={formErrors.message ? true : false} onChange={()=>clearError('message')} />
+                placeholder="Write your message here" type='email' size='big' error={formErrors.message ? true : false} onChange={() => clearError('message')} />
             <button type="submit" className={styles.contactSubmitButton}>
                 Submit
             </button>
